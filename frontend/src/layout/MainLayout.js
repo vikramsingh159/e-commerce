072173da64b2e8aa04cb7f55f9690e19980ba537
Vikram Singh <er.vikramsingh10@gmail.com>
2023-01-31 17:20:06 +0530

    mern stack tsirt shop app

---
+++ frontend/src/layout/MainLayout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import RoutingConfig from '../navigation/RoutingConfig'
+
+const MainLayout = () => {
+    return (
+        <div><RoutingConfig /></div>
+    )
+}
+
+export default MainLayout
---
+++ frontend/src/navigation/RoutingConfig.js
@@ -0,0 +1,43 @@
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './../core/Home';
+import Signup from './../user/Signup';
+import Signin from './../user/Signin';
+import Cart from './../core/Cart';
+import UserDashBoard from './../user/UserDashBoard';
+import PrivateRoute from './../auth/helper/PrivateRoutes';
+import AdminRoute from './../auth/helper/AdminRoutes';
+import AdminDashBoard from './../user/AdminDashBoard';
+import AddCategory from './../admin/AddCategory';
+import ManageCategories from './../admin/ManageCategories';
+import AddProduct from './../admin/AddProduct';
+import UpdateProduct from './../admin/UpdateProduct';
+import ManageProducts from './../admin/ManageProducts';
+import UpdateCategory from './../admin/UpdateProduct';
+
+
+
+const RoutingConfig = () => {
+    return (
+        <Router>
+
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/signup" exact component={Signup} />
+                <Route path="/signin" exact component={Signin} />
+                <Route path="/cart" exact component={Cart} />
+                <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
+                <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+                <AdminRoute path="/admin/create/category" exact component={AddCategory} />
+                <AdminRoute path="/admin/categories" exact component={ManageCategories} />
+                <AdminRoute path="/admin/create/product" exact component={AddProduct} />
+                <AdminRoute path="/admin/products" exact component={ManageProducts} />
+                <AdminRoute path="/admin/product/update/:productId" exact component={UpdateProduct} />
+                <AdminRoute path="/admin/category/update/:categoryId" exact component={UpdateCategory} />
+            </Switch>
+
+        </Router>
+    )
+}
+
+export default RoutingConfig
